a529c30b994ac9a8cae4223d283a4b8774e2519e
aodwodud96 <aodwodud96@gmail.com>
2026-02-03 14:10:20 +0000

    Initial commit

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-// Add JS here
+const lottoNumbersContainer = document.getElementById('lotto-numbers-container');
+const generateBtn = document.getElementById('generate-btn');
+
+function generateLottoNumbers() {
+    lottoNumbersContainer.innerHTML = '';
+    const numbers = new Set();
+    while (numbers.size < 6) {
+        numbers.add(Math.floor(Math.random() * 49) + 1);
+    }
+
+    for (const number of numbers) {
+        const lottoNumber = document.createElement('div');
+        lottoNumber.classList.add('lotto-number');
+        lottoNumber.textContent = number;
+        lottoNumbersContainer.appendChild(lottoNumber);
+    }
+}
+
+generateBtn.addEventListener('click', generateLottoNumbers);
+
+generateLottoNumbers();
